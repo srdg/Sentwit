ae6b6c7346f07460ed4fea56256ab0c36450ccf1
Soumik Ranjan Dasgupta <dasguptaarjo@gmail.com>
2018-05-06 15:45:00 +0530

    Initial commit

---
+++ keys.py
@@ -0,0 +1,8 @@
+# Warning : Keep this file private
+
+# replace the values of the variables with yours
+
+ckey = 'SaMpLe-C0nSuMeR-Key'
+csecret = 'Y0uR-C0nSuMeR-SecRet'
+atoken = 'Y0Ur-@cCeSs-T0Ken'
+asecret = 'Y0uR-@cCesS-SeCrEt'
