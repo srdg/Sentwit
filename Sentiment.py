c279a2a64dd5d0a6eddf5c48408ddc7e42cca7e1
Soumik Ranjan Dasgupta <20687315+srdg@users.noreply.github.com>
2020-09-30 14:50:00 +0000

    Major upgrade to web-app

--- Sentiment.py
+++ Sentiment.py
@@ -1,113 +1,35 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
-import time
+import tweepy
 from tweepy import Stream
-from tweepy import OAuthHandler
-from tweepy.streaming import StreamListener
-import json
-from textblob import TextBlob
-import matplotlib.pyplot as plt
-import re
+from text2emotion import get_emotion
+import streamlit as st
+import numpy as np
+import pandas as pd
+import time
 
+auth = tweepy.OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
 
+api = tweepy.API(auth)
 
-# In[2]:
+count = 100
 
+class StreamListener(tweepy.StreamListener):
 
-def calctime(a):
-    return time.time()-a
-
-
-# In[3]:
-
-
-positive=0
-negative=0
-compound=0
-
-
-# In[4]:
-
-
-count=0
-initime=time.time()
-plt.ion()
-
-
-# In[5]:
-
-
-import keys
-
-ckey=keys.ckey
-csecret=keys.csecret
-atoken=keys.atoken
-asecret=keys.asecret
-
-
-# In[6]:
-
-
-class listener(StreamListener):
+    def __init__(self, time_limit=10):
+        self.start_time=time.time()
+        self.time_limit=time_limit
+        super(StreamListener, self).__init__()
     
-    def on_data(self,data):
-        global initime
-        t=int(calctime(initime))
-        all_data=json.loads(data)
-
-        tweet=all_data["text"]
-
-        tweet=" ".join(re.findall("[a-zA-Z]+", tweet))
-        blob=TextBlob(tweet.strip())
-
-        global positive
-        global negative     
-        global compound  
-        global count
-        
-        count=count+1
-        senti=0
-        for sen in blob.sentences:
-            senti=senti+sen.sentiment.polarity
-            if sen.sentiment.polarity >= 0:
-                positive=positive+sen.sentiment.polarity   
-            else:
-                negative=negative+sen.sentiment.polarity  
-        compound=compound+senti        
-        
-        print ('#'+str(count) + ":"+ tweet.strip())
-
-        
-    
-        plt.axis([ 0, 200, -20,20])
-        plt.xlabel('Time')
-        plt.ylabel('Sentiment')
-        plt.plot([t],[positive],'go--',[t] ,[negative],'ro',[t],[compound],'bo')
-        plt.draw()
-        plt.pause(0.05)
-        if count==200:
-            return False
-        else:
+    def on_status(self, status):
+        if (time.time() - self.start_time) < self.time_limit:
+            print(status.text)
+            print("***************************Emotions detected******************************************")
+            print(get_emotion(status.text))
             return True
-        
-    def on_error(self,status):
-        print (status)
+        else:
+            return False
 
+listener = StreamListener()
+stream = tweepy.Stream(auth = api.auth, listener=listener)
 
-# In[7]:
-
-
-auth=OAuthHandler(ckey,csecret)
-auth.set_access_token(atoken,asecret)
-
-
-# In[8]:
-
-
-twitterStream=  Stream(auth, listener(count))
-twitterStream.filter(track=["Narendra Modi"])
-
+stream.filter(track=['Presidential Debate'])
